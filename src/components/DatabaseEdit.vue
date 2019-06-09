e7f12d8eb18861cea11f08832f20355fb130dad4
Marco Comes <comes95@gmail.com>
2019-06-09 16:35:51 +0200

    Implement database creation page

--- src/components/DatabaseEdit.vue
+++ src/components/DatabaseEdit.vue
@@ -1,25 +1,82 @@
 <template>
-    <div class="main">
+    <form id="create-db" @submit.prevent="persistDb()">
         <div class="form-group">
             <label class="form-control-desc" for="name">Database name</label>
-            <input id="name" class="form-control" type="text" placeholder="Name...">
+            <div class="form-error" v-if="name.error">{{ name.error }}</div>
+            <input
+                id="name"
+                class="form-control"
+                :class="{ 'mod-error': name.error }"
+                type="text"
+                placeholder="Name..."
+                v-model="name.value">
         </div>
         <div class="form-group">
             <label class="form-control-desc" for="password">Database password</label>
-            <input id="password" class="form-control" type="text" placeholder="Password...">
+            <div class="form-error" v-if="password.error">{{ password.error }}</div>
+            <input
+                id="password"
+                class="form-control"
+                :class="{ 'mod-error': password.error }"
+                type="password"
+                placeholder="Password..."
+                v-model="password.value">
         </div>
         <div class="form-group mod-action">
-            <button class="form-action">Save changes</button>
+            <button class="form-action" type="submit">Save changes</button>
         </div>
-    </div>
+    </form>
 </template>
 
 <script>
-export default {
+import { Database } from '../db.js';
 
+export default {
+    data: () => ({
+        name: {
+            value: '',
+            error: null,
+        },
+        password: {
+            value: '',
+            error: null,
+        },
+    }),
+    methods: {
+        validateName() {
+            if (this.name.value === '') {
+                this.name.error = 'Name required';
+            } else {
+                this.name.error = null;
+            }
+        },
+        validatePassword() {
+            if (this.password.value.length < 8) {
+                this.password.error = '8 characters min';
+            } else {
+                this.password.error = null;
+            }
+        },
+        persistDb() {
+            this.validateName();
+            this.validatePassword();
+
+            if (!this.name.error && !this.password.error) {
+                Database(this.name.value, this.password.value)
+                    .then((db) => {
+                        return db.save();
+                    })
+                    .then(() => {
+                        this.$emit('pageChange', 'Index');
+                    })
+                    .catch(() => {
+
+                    });
+            }
+        }
+    }
 }
 </script>
 
 <style>
-
 </style>
