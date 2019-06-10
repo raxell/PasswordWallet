ad8dabb3b8aee755e6aa23ae7f1aaa402ed4810e
Marco Comes <comes95@gmail.com>
2019-06-10 16:13:52 +0200

    Share CSS of boxes

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -6,17 +6,19 @@
             </button>
         </div>
 
-        <ul class="dbs" v-if="dbs.length > 0">
-            <li class="db" v-for="db in dbs" :key="db">
-                <h2 class="db-name" @click="$emit('pageChange', 'Database', { db })">
-                    {{ db }}
-                </h2>
-                <Edit2Icon
-                    class="db-action mod-edit"
-                    @click="$emit('pageChange', 'DatabaseEdit', { db, action: 'edit' })" />
-                <Trash2Icon
-                    class="db-action mod-delete"
-                    @click="$emit('pageChange', 'DatabaseEdit', { db, action: 'delete' })" />
+        <ul class="boxes" v-if="dbs.length > 0">
+            <li class="box" v-for="db in dbs" :key="db">
+                <div class="box-item">
+                    <h2 class="item-name mod-title" @click="$emit('pageChange', 'Database', { db })">
+                        {{ db }}
+                    </h2>
+                    <Edit2Icon
+                        class="item-action mod-edit"
+                        @click="$emit('pageChange', 'DatabaseEdit', { db, action: 'edit' })" />
+                    <Trash2Icon
+                        class="item-action mod-delete"
+                        @click="$emit('pageChange', 'DatabaseEdit', { db, action: 'delete' })" />
+                </div>
             </li>
         </ul>
     </div>
@@ -33,70 +35,10 @@ export default {
     components: {
         Edit2Icon,
         Trash2Icon,
+        CopyIcon,
     }
 }
 </script>
 
 <style lang="scss">
-@import '../assets/variables.scss';
-
-.dbs {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-}
-
-.db {
-    width: 100%;
-    box-shadow: 0 0 1.5rem rgba(0, 0, 0, .1);
-    border-radius: 1rem;
-    display: flex;
-    align-items: center;
-    margin-bottom: 3rem;
-}
-
-.db-name {
-    font-size: 1.6rem;
-    padding: 2rem;
-    width: 100%;
-    cursor: pointer;
-    margin: 0;
-
-    &:hover {
-        text-decoration: underline;
-    }
-}
-
-.db-action {
-    cursor: pointer;
-    box-sizing: content-box;
-    padding: 1rem;
-    border-radius: .5rem;
-
-    &:last-of-type {
-        margin-right: 1rem;
-    }
-
-    &:hover {
-        box-shadow: 0 0 1rem rgba(0, 0, 0, .1);
-    }
-
-    &.mod-edit {
-        color: $color-blue;
-
-        &:hover {
-            background: $bg-blue;
-        }
-    }
-
-    &.mod-delete {
-        color: $color-red;
-
-        &:hover {
-            background: $bg-red;
-        }
-    }
-}
-
-
 </style>
